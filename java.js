bf2e0d1513127e81c35e6df657d30428a2a4544a
Vgiaimo <vgiaimo@hawk.iit.edu>
2017-09-21 14:10:58 -0500

    Updated files
    I added a text highlight feature using java. Also, I have added another media query for a better responsive webpage.

--- java.js
+++ java.js
@@ -1,4 +1,19 @@
-function button(){
+function button()
+{
 	alert("Thank you for Subscribing!");
 	return true;
+}
+
+function bigtext()
+{
+	var fname = document.getElementById("firstName");
+	fname.value = fname.value.toUpperCase();
+
+	var lname = document.getElementById("lastName");
+	lname.value = lname.value.toUpperCase();
+
+}
+
+function highlight(textB) {
+    textB.style.background = "#8cf442";
 }
