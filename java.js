357cbd5a2acb01568c8e0fdd6e282e1154c06a97
Vgiaimo <vgiaimo@hawk.iit.edu>
2017-10-06 10:29:23 -0500

    updated Html and CSS
    I created a smaller image to be inserted when the screen size gets smaller.
    
    I got the input boxes to aliagn, but still no luck with aligning the labels.

--- java.js
+++ java.js
@@ -1,19 +1,20 @@
 function button()
-{
+  {
 	alert("Thank you for Subscribing!");
 	return true;
-}
+	}
+
 
 function bigtext()
-{
+	{
 	var fname = document.getElementById("firstName");
 	fname.value = fname.value.toUpperCase();
 
 	var lname = document.getElementById("lastName");
 	lname.value = lname.value.toUpperCase();
 
-}
+	}
 
 function highlight(textB) {
-    textB.style.background = "#8cf442";
-}
+	textB.style.background = "#8cf442";
+	}
